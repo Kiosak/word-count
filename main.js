463a5c576bf2c7c628d6e183475884a51918e117
Vadym <40339893+Kiosak@users.noreply.github.com>
2019-03-03 21:05:11 +0100

    new structure

--- main.js
+++ main.js
@@ -1,58 +1,67 @@
-// Set the maximum number of words in the textarea field.
-let maxWords = 200;
-
-// Executes on .card click
-// Listen only to the focused textarea for all keyboard events.
-$('.card').click(function(event) {
-  const target = $(event.target);
-  
-  if (target.is('textarea')) {
-    target.keydown(calcucalteWords);
-  }
-});
-
-// Count the number of words
-function calcucalteWords(event) {
-  // A temporary array for the maximum number of words to prevent an over limit.
-  // Executes only when the maximum number of words is reached.
-  let maxWordsArr;
-
-  // Get the entered data
-  // Trim the words into an array
-  // Count words
-  const getInput = event.currentTarget.value;
-  const words = getInput.trim().split(/\s+/);
-  const wordsArr = words.map(x => x != '');
-  const len = wordsArr.length;
-  // Check the last element in wordsArr, if it is equal to an empty string, avoid counting this element
-  const countWords = wordsArr[len - 1] == '' ? len - 1 : len;
-  
-
-  // Get the output field
-  const outputCount = event.target.parentElement.parentNode.children[2].children[0];
-  const outputLine = event.target.parentElement.parentNode.childNodes[5];
-
-  // Check the number of words entered limit of words
-  if (countWords >= maxWords) {
-    // Avoid input over limit and copy paste
-    if (event.which === 86 || event.which === 32) {
-      event.preventDefault();
-    }
-    // Set warning color when limit of words is reached
-    $(outputLine)
-      .removeClass('text-secondary')
-      .addClass('text-danger');
-
-    // Cut extra words
-    maxWordsArr = words.slice(0, maxWords).join(' ');
-    event.currentTarget.value = maxWordsArr;
-  } else {
-    // Set the normal color when below the word limit.
-    $(outputLine)
-      .removeClass('text-danger')
-      .addClass('text-secondary');
-  }
-
-  // Display counted words
-  outputCount.innerText = countWords;
+let app = {
+  maxWords: 200,
+  counted: 0,
+  ctrl: false,
+  shift: false,
 }
+
+$('textarea').keyup(react).keydown(block);
+
+function react(e) {
+  const str = $('textarea').val();
+  app.counted = calc(str);
+  if (e.which === 17) {
+    app.ctrl = false;
+  }
+  if (e.which === 16) {
+    app.shift = false;
+  } 
+  
+  $('span').text(app.counted); // display counted
+
+  if (app.counted >= app.maxWords) { // colorised output
+    $('.card-text').removeClass('text-secondary').addClass('text-danger');
+  } else {
+    $('.card-text').removeClass('text-danger').addClass('text-secondary');
+  }
+} 
+
+function block(e) {
+  const str = $('textarea').val();
+  app.counted = calc(str);
+  const key = e.which;
+  if (key === 16) {
+    app.shift = true;
+  }
+  if (app.counted >= app.maxWords ) {
+    const wordChar = String.fromCharCode(key).search(/\w/);
+    const specSymbol = String.fromCharCode(key).search(/\d/);
+    const other = key.toString().search(/(^[1-9]{1}$|^[1-4]{1}[0-9]{1}$|^50$)/);
+    if (key === 13 || key === 32 || (specSymbol === 0 && app.shift) || (other!== 0 && wordChar !== 0)) {
+      e.preventDefault()
+    } 
+  }
+}
+
+function calc(str) {  
+  arr = str.trim().match(/([A-z0-9'-]+)/g); // put words in array
+  arr === null ? arr=[] : arr = arr; // remove empty string 
+  return arr.length; 
+}
+
+const stopPasting = event => {
+  const str = event.clipboardData.getData('text');
+  const len = calc(str)
+  if (app.counted > 0) {
+    app.counted = app.counted + len; // prevent pasting over limit 
+  } else {
+    app.counted = len;
+  }
+  
+	if (app.counted >= app.maxWords) {
+    alert('You are trying to insert too many words!');
+		return event.preventDefault();
+  }
+};
+
+window.addEventListener('paste', stopPasting);
